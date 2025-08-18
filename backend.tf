4cfeffcee52160344da06663d8ff676b7827e684
Kevin Goehring <you@example.com>
2025-08-18 11:24:58 -0500

    Configure gitignore and backend file

--- backend.tf
+++ backend.tf
@@ -1,1 +1,9 @@
-
+terraform {
+  backend "s3" {
+    bucket         = "tf-state-850924742419-dev"
+    key            = "rds-redis/dev/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "tf-locks-dev"
+    encrypt        = true
+  }
+}
