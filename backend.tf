924ea85bc934e98e097ba4ad41a2c347c7fffc2a
Kevin Goehring <you@example.com>
2025-08-19 00:43:15 -0500

    Fix error in bucket name

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.11.0"
   backend "s3" {
-    bucket       = "tf-state-<acct>-dev"
+    bucket       = "tf-state-850924742419-dev"
     key          = "rds-redis/dev/terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
