aa9096533f154a934289cee8251a35e45b8459cd
Kevin Goehring <you@example.com>
2025-08-19 00:14:46 -0500

    Update backend / remove dynamodb

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "tf-state-850924742419-dev"
-    key            = "rds-redis/dev/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "tf-locks-dev"
-    encrypt        = true
+    bucket       = "tf-state-850924742419-dev"
+    key          = "rds-redis/dev/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
